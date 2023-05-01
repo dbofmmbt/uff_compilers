56ff00afd238e843ecacda8b98941607b07caa0c
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2023-05-01 15:59:55 -0300

    Add more test cases for prolog

--- tests/test_prolog.py
+++ tests/test_prolog.py
@@ -6,7 +6,12 @@ from tests.util import check
 prolog_spec = r"""
 LEFT_PAREN \( IDK
 RIGHT_PAREN \) IDK
+
 QUERY ?- IDK
+DOT . IDK
+COMMA , IDK
+RULE :- IDK
+
 VARIABLE \A(\a+\A)* IDK
 ATOM \a(\a+\A)* IDK
 NUMBER (1+2+3+4+5+6+7+8+9)(0+1+2+3+4+5+6+7+8+9)* IDK
@@ -40,5 +45,120 @@ def test_number_with_others():
         VARIABLE Variable
         NUMBER 128
         ATOM atom
+        """,
+    )
+
+
+def test_fact():
+    check(
+        prolog,
+        "food(burger).",
+        """
+        ATOM food
+        LEFT_PAREN (
+        ATOM burger
+        RIGHT_PAREN )
+        DOT .
+        """,
+    )
+
+
+def test_rule():
+    check(
+        prolog,
+        "meal(X) :- food(X).",
+        """
+        ATOM meal
+        LEFT_PAREN (
+        VARIABLE X
+        RIGHT_PAREN )
+        RULE :-
+        ATOM food
+        LEFT_PAREN (
+        VARIABLE X
+        RIGHT_PAREN )
+        DOT .
+        """,
+    )
+
+
+def test_query():
+    check(
+        prolog,
+        "?- food(pizza).",
+        """
+        QUERY ?-
+        ATOM food
+        LEFT_PAREN (
+        ATOM pizza
+        RIGHT_PAREN )
+        DOT .
+        """,
+    )
+
+
+def test_query_with_more_than_one_clause():
+    check(
+        prolog,
+        "?- meal(X), lunch(X).",
+        """
+        QUERY ?-
+        ATOM meal
+        LEFT_PAREN (
+        VARIABLE X
+        RIGHT_PAREN )
+        COMMA ,
+        ATOM lunch
+        LEFT_PAREN (
+        VARIABLE X
+        RIGHT_PAREN )
+        DOT .
+        """,
+    )
+
+
+def test_professor_query():
+    check(
+        prolog,
+        "professor(X, Y) :- teaches(X, C), studies(Y, C).",
+        """
+        ATOM professor
+        LEFT_PAREN (
+        VARIABLE X
+        COMMA ,
+        VARIABLE Y
+        RIGHT_PAREN )
+        RULE :-
+        ATOM teaches
+        LEFT_PAREN (
+        VARIABLE X
+        COMMA ,
+        VARIABLE C
+        RIGHT_PAREN )
+        COMMA ,
+        ATOM studies
+        LEFT_PAREN (
+        VARIABLE Y
+        COMMA ,
+        VARIABLE C
+        RIGHT_PAREN )
+        DOT .
+        """,
+    )
+
+
+def test_students_query():
+    check(
+        prolog,
+        "?- professor(kirke, Students).",
+        """
+        QUERY ?-
+        ATOM professor
+        LEFT_PAREN (
+        ATOM kirke
+        COMMA ,
+        VARIABLE Students
+        RIGHT_PAREN )
+        DOT .
         """,
     )
