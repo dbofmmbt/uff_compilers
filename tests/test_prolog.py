e72d019bdb9a3d04c1e595b000fab527624b26b0
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2023-05-01 12:13:41 -0300

    Refactor prolog test to use new escape

--- tests/test_prolog.py
+++ tests/test_prolog.py
@@ -7,8 +7,8 @@ prolog_spec = r"""
 LEFT_PAREN \( IDK
 RIGHT_PAREN \) IDK
 QUERY ?- IDK
-VARIABLE (A+B+C+D+E+F+G+H+I+J+K+L+M+N+O+P+Q+R+S+T+U+V+W+X+Y+Z)((A+B+C+D+E+F+G+H+I+J+K+L+M+N+O+P+Q+R+S+T+U+V+W+X+Y+Z+a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)*) IDK
-ATOM (a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)((A+B+C+D+E+F+G+H+I+J+K+L+M+N+O+P+Q+R+S+T+U+V+W+X+Y+Z+a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z)*) IDK
+VARIABLE \A((\a+\A)*) IDK
+ATOM \a((\a+\A)*) IDK
 NUMBER (1+2+3+4+5+6+7+8+9)((0+1+2+3+4+5+6+7+8+9)*) IDK
 """
 
